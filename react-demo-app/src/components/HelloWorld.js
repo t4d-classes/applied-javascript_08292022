ad143ebb139f96979d4b744fce3c5507c7a872c2
Eric Greene <eric@t4d.io>
2022-08-30 12:51:45 -0400

    add exercise 1

---
+++ react-demo-app/src/components/HelloWorld.js
@@ -0,0 +1,15 @@
+export const HelloWorld = () => {
+
+  // JSX
+  // return React.createElement(
+  //   React.Fragment, null,
+  //   React.createElement('h1', null, 'Hello'),
+  //   React.createElement('span', null, 'test')));
+  return (
+    <>
+      <h1>Hello</h1>
+      <span>test</span>
+    </>
+  );
+
+};
