8dce584b89442e5f866d180024293a4297f62768
Eric Greene <eric@t4d.io>
2022-08-31 19:56:33 -0400

    add exercise 2

---
+++ angular-demo-app/src/app/car-tool/components/car-home/car-home.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarHomeComponent } from './car-home.component';
+
+describe('CarHomeComponent', () => {
+  let component: CarHomeComponent;
+  let fixture: ComponentFixture<CarHomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CarHomeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CarHomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- angular-demo-app/src/app/color-tool/components/color-home/color-home.component.ts
+++ angular-demo-app/src/app/color-tool/components/color-home/color-home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Color } from '../../models/colors';
+
 @Component({
   selector: 'app-color-home',
   templateUrl: './color-home.component.html',
@@ -9,7 +11,14 @@ export class ColorHomeComponent implements OnInit {
 
   headerText = "Color Tool"
 
-  constructor() { }
+  colors: Color[] = [
+    { id: 1, name: 'red', hexcode: 'ff0000' },
+    { id: 2, name: 'green', hexcode: '00ff00' },
+    { id: 3, name: 'blue', hexcode: '0000ff' },
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
